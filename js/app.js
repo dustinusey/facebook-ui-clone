7ef22a23e37c9917ebff97494486facb0901d651
Dustin Usey <dustinusey@me.com>
2020-08-23 13:07:58 -0500

    nav styles for light/dark mode complete

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@ const dot = document.querySelector('.dot');
 const body = document.querySelector('body');
 dot.addEventListener('click', () => {
     body.classList.toggle('dark');
+    if (body.classList.contains('dark')) {
+        dot.style.backgroundColor = 'white'
+    } else {
+        dot.style.backgroundColor = 'rgb(50, 55, 63)';
+    }
 })
